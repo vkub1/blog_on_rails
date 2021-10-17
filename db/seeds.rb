232ad8f659839858a468e6789c4c704fd1642608
vkub1 <kubliyvlad@gmail.com>
2021-10-16 22:46:45 -0700

    Added and tested comments model validation and modified seed file to generate comments

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,23 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Comment.destroy_all
 Post.destroy_all
+
 
 50.times do
     created_at = Faker::Date.backward(days:365 * 5)
-    Post.create(
+    post = Post.create(
         title: Faker::Book.title,
         body: Faker::Lorem.paragraph_by_chars(number: rand(51..200), supplemental: false)
     )
+    if post.valid?
+        rand(1..5).times do
+            Comment.create(
+                body: Faker::Lorem.paragraph_by_chars(number: rand(51..200), supplemental: false),
+                post: post
+            )
+        end
+    end
 end
 
